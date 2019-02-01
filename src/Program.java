764bd1fc4431311c8e0ee3f279387a411fa2c225
CodyvanderPoel <cvanderpoel@basecampcodingacademy.org>
2019-02-01 09:41:51 -0600

    Edited how the program now runs.
    I added imports, and a while loop with conditions inside of it that now allow the user determine how it will run.

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
-    public static void main (String[] args) {
+
+    public static void main (String[] args) throws IOException {
+        Scanner user = new Scanner(System.in);
 //     Commented out for development   LetterGrader lg = new LetterGrader();
 //        lg.grade();
-        GradeAverager ga = new GradeAverager();
-        ga.gatherGrades();
+        while (true) {
+            System.out.println("\n[1]-- Average new set of grades\n[2]-- View Grades\n[3]-- Closes program");
+            String input = user.nextLine();
+            if (input.equals("1")) {
+                GradeAverager ga = new GradeAverager();
+                ga.gatherGrades();
+            } else if (input.equals("2")) {
+                GradeBook gb = new GradeBook();
+                gb.readGrades();
+            } else if (input.equals("3")) {
+                System.exit(0);
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
     }
 }
